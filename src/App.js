fe604b5e9495b329718c028e09044c0289de2499
瀬戸口純也 <junya.setoguchi@pixta.co.jp>
2020-10-27 22:52:51 +0900

    my first React Hook with useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+  const increment2 = () => setCount(previousCount => previousCount + 1)
+  const decrement2 = () => setCount(previousCount => previousCount - 1)
+  const doubleCount = () => setCount(previousCount => previousCount * 2)
+  const devideCount3 = () => setCount(previousCount => {
+    if (previousCount % 3 === 0) {
+      return (
+        previousCount / 3
+      );
+    }
+    else {
+      return (
+        previousCount
+      );
+    }
+  })
+  const devideCount3_2 = () => setCount(previousCount => {
+    return (
+      previousCount % 3 === 0 ? previousCount / 3 : previousCount
+    )
+  })
+  const resetCount = () => setCount(0)
+
   return (
-    <div className="">
-      This is a template for React app
-    </div>
+    <React.Fragment>
+      <div className="">
+        count: {count}
+      </div>
+      <div className="">
+        <button className="" onClick={increment}>
+          +1
+        </button>
+        <button className="" onClick={decrement}>
+          -1
+        </button>
+      </div>
+      <div className="">
+        <button className="" onClick={increment2}>
+          +1
+        </button>
+        <button className="" onClick={decrement2}>
+          -1
+        </button>
+      </div>
+      <div className="">
+        <button className="" onClick={doubleCount}>
+          ×2
+        </button>
+      </div>
+      <div className="">
+        <button className="" onClick={devideCount3}>
+          ３の倍数の時だけ３で割る
+        </button>
+      </div>
+      <div className="">
+        <button className="" onClick={devideCount3_2}>
+          ３の倍数の時だけ３で割る
+        </button>
+      </div>
+      <div className="">
+        <button className="" onClick={resetCount}>
+          Reset
+        </button>
+      </div>
+    </React.Fragment>
   );
 }
 
