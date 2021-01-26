ebe5f600f76333ef9d92037912f6a1975c9e7ad7
瀬戸口純也 <junya.setoguchi@pixta.co.jp>
2021-01-26 21:48:27 +0900

    implement component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,75 +1,46 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const App = (props) => {
 
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
-  const increment2 = () => setCount(previousCount => previousCount + 1)
-  const decrement2 = () => setCount(previousCount => previousCount - 1)
-  const doubleCount = () => setCount(previousCount => previousCount * 2)
-  const devideCount3 = () => setCount(previousCount => {
-    if (previousCount % 3 === 0) {
-      return (
-        previousCount / 3
-      );
-    }
-    else {
-      return (
-        previousCount
-      );
-    }
-  })
-  const devideCount3_2 = () => setCount(previousCount => {
-    return (
-      previousCount % 3 === 0 ? previousCount / 3 : previousCount
-    )
-  })
-  const resetCount = () => setCount(0)
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
+
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
 
   return (
     <React.Fragment>
-      <div className="">
-        count: {count}
-      </div>
-      <div className="">
-        <button className="" onClick={increment}>
-          +1
-        </button>
-        <button className="" onClick={decrement}>
-          -1
-        </button>
-      </div>
-      <div className="">
-        <button className="" onClick={increment2}>
-          +1
-        </button>
-        <button className="" onClick={decrement2}>
-          -1
-        </button>
-      </div>
-      <div className="">
-        <button className="" onClick={doubleCount}>
-          ×2
-        </button>
-      </div>
-      <div className="">
-        <button className="" onClick={devideCount3}>
-          ３の倍数の時だけ３で割る
-        </button>
-      </div>
-      <div className="">
-        <button className="" onClick={devideCount3_2}>
-          ３の倍数の時だけ３で割る
-        </button>
-      </div>
-      <div className="">
-        <button className="" onClick={resetCount}>
-          Reset
-        </button>
-      </div>
+      <p className="">
+        現在の{name}は、{price}です。
+      </p>
+      <button className="" onClick={() => setPrice(price + 1)}>
+        +1
+      </button>
+      <button className="" onClick={() => setPrice(price - 1)}>
+        -1
+      </button>
+      <button className="" onClick={() => reset()}>
+        Reset
+      </button>
+      <input
+        type="text"
+        name=""
+        value={name}
+        placeholder=""
+        className=""
+        onChange={
+          (e) => setName(e.target.value)
+        }
+      />
     </React.Fragment>
   );
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default App;
